9643d8af2b8ec26ab3b8a9a686fbc8d8a8242247
Goldie Tiara <goldietiara.acc@gmail.com>
2023-08-15 00:22:35 +0700

    prisma extension

--- src/app/products/[id]/actions.ts
+++ src/app/products/[id]/actions.ts
@@ -10,16 +10,27 @@ export async function incrementProductQuantity(productId: string) {
   const productInCart = cart.items.find((item) => item.productId === productId);
 
   if (productInCart) {
-    await prisma.cartItem.update({
-      where: { id: productInCart.id },
-      data: { quantity: { increment: 1 } },
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: {
+          update: {
+            where: { id: productInCart.id },
+            data: { quantity: { increment: 1 } },
+          },
+        },
+      },
     });
   } else {
-    await prisma.cartItem.create({
+    await prisma.cart.update({
+      where: { id: cart.id },
       data: {
-        cartId: cart.id,
-        productId,
-        quantity: 1,
+        items: {
+          create: {
+            productId,
+            quantity: 1,
+          },
+        },
       },
     });
   }
