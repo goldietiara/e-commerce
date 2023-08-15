0b405170c799c1fe9fcacfce872e79fe8740257e
Goldie Tiara <goldietiara.acc@gmail.com>
2023-08-15 18:16:46 +0700

    update hero design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,30 +25,55 @@ export default async function Home({
     take: pageSize + (currentPage === 1 ? heroItemCount : 0),
   });
   return (
-    <main className=" flex flex-col items-center">
+    <main className=" flex flex-col items-center lg:mx-20">
       {currentPage === 1 && (
-        <div className=" hero rounded-2xl bg-base-200">
-          <div className=" hero-content flex-col md:flex-row">
-            <Image
-              src={products[0].imageUrl}
-              alt={products[0].name}
-              width={400}
-              height={800}
-              className="w-full max-w-sm rounded-lg shadow-2xl"
-              priority
-            />
-            <div>
-              <h1 className="text-5xl font-bold">{products[0].name}</h1>
-              <p className="py-6">{products[0].description}</p>
+        <div className=" relative h-[150px] w-full grid-cols-1 overflow-hidden rounded-xl bg-pink-200 shadow-xl md:h-[350px]">
+          <Image
+            src={products[0].imageUrl}
+            alt={products[0].name}
+            width={1000}
+            height={1000}
+            className=" col-span-1 w-full object-center"
+            priority
+          ></Image>
+          <div className="absolute top-0 h-full w-full bg-black/20">
+            <div className="flex h-full w-full flex-col items-center justify-center px-5">
+              <h1 className=" text-xl font-bold text-base-100 md:text-5xl">
+                {products[0].name}
+              </h1>
+              <p className=" py-2 text-center text-sm text-base-100 md:py-6 md:text-base">
+                {products[0].description}
+              </p>
               <Link
                 href={`/products/${products[0].id}`}
-                className="btn-primary btn"
+                className=" btn-sm btn bg-base-100 text-xs text-gray-700 md:btn md:text-base"
               >
-                Check it out
+                ORDER NOW
               </Link>
             </div>
           </div>
         </div>
+        //   <div className=" hero-content flex-col md:flex-row">
+        //     <Image
+        //       src={products[0].imageUrl}
+        //       alt={products[0].name}
+        //       width={400}
+        //       height={800}
+        //       className="w-full max-w-sm rounded-lg shadow-2xl"
+        //       priority
+        //     />
+        //     <div>
+        //       <h1 className="text-5xl font-bold">{products[0].name}</h1>
+        //       <p className="py-6">{products[0].description}</p>
+        //       <Link
+        //         href={`/products/${products[0].id}`}
+        //         className="btn-primary btn"
+        //       >
+        //         ORDER NOW
+        //       </Link>
+        //     </div>
+        //   </div>
+        // </div>
       )}
       <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
         {(currentPage === 1 ? products.slice(1) : products).map((products) => {
