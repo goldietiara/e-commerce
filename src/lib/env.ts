2c4a30cee661f7ed15a15a48a6850113c9e0750c
Goldie Tiara <goldietiara.acc@gmail.com>
2023-08-15 14:08:16 +0700

    next auth typo

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,8 +4,8 @@ const envSchema = zod.object({
   DATABASE_URL: zod.string().nonempty(),
   GOOGLE_CLIENT_ID: zod.string().nonempty(),
   GOOGLE_CLIENT_SECRET: zod.string().nonempty(),
-  NEXT_AUTH_URL: zod.string().nonempty(),
-  NEXT_AUTH_SECRET: zod.string().nonempty(),
+  NEXTAUTH_URL: zod.string().nonempty(),
+  NEXTAUTH_SECRET: zod.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
